63c91c968040a46f3ea46c2c98c5a0745467d6e2
Admin <baro@ikrt>
2016-02-22 15:35:27 +0200

    Lab2.1.2

---
+++ src/edu/brainacad/Main.java
@@ -0,0 +1,33 @@
+package edu.brainacad;
+
+/**
+ * Created by Admin on 22.02.2016.
+ */
+public class Main {
+
+    private String ManuFacture;
+    private int serialNumber;
+    private float price;
+    private int quantityCPU;
+    private int frequencyCPU;
+
+    public String setManuFacture(){
+        return ManuFacture;
+    }
+    public int setSerialNumber(){
+        return serialNumber;
+    }
+
+    public float setPrice(){
+        return price;
+    }
+    public int getQuantityCPU(){
+        return quantityCPU;
+    }
+    public int getFrequencyCPU(){
+        return frequencyCPU;
+    }
+
+
+
+}
